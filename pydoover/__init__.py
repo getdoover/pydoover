276509cef4407e209f2309be0b3d6a63ae00503b
mathsman5133 <mathsman5132@gmail.com>
2025-06-02 16:38:45 +1000

    Bump version to 0.4.1a and fix an import to allow lazy importing of modules without dependencies installed

--- pydoover/__init__.py
+++ pydoover/__init__.py
@@ -1,10 +1,1 @@
-from . import (
-    cli as cli,
-    cloud as cloud,
-    config as config,
-    docker as docker,
-    reports as reports,
-    state as state,
-    ui as ui,
-    utils as utils,
-)
+from . import *  # noqa: F403
