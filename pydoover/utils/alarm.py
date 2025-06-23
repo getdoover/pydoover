f2816ddaa35f06701551a00c21e43a0ae3f80309
Sidljackson <sid@span-eng.com>
2025-06-23 16:42:09 +1000

    removed explicit decorator-ness

--- pydoover/utils/alarm.py
+++ pydoover/utils/alarm.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
 import time
-import logging
-from functools import wraps
 
 ## A generic alarm class that can be used to trigger things via a callback function when a threshold is met
 ## threshold can be greater than or less than a specified value
@@ -17,81 +15,44 @@ from functools import wraps
 class Alarm:
     def __init__(
         self,
-        threshold=None,
+        threshold_met,
         callback=None,
-        direction=None,
         grace_period=None,
         min_inter_alarm=None,
     ):
-        self.debug = False
-        
-        self.default_direction = "greater_than"
-        self.default_grace_period = 60 * 60 #an hour
-        self.default_min_inter_alarm = 60 * 60 * 24 #a day
+        self.default_grace_period = 60 * 60  # an hour
+        self.default_min_inter_alarm = 60 * 60 * 24  # a day
 
-        self.threshold = threshold
+        self.threshold_met = threshold_met
         self.callback = callback
-        self.direction = direction or self.default_direction
         self.grace_period = grace_period or self.default_grace_period
         self.min_inter_alarm = min_inter_alarm or self.default_min_inter_alarm
 
         self.last_alarm_time = None
         self.initial_trigger_time = None
 
-    def set_threshold(self, threshold):
-        self.threshold = threshold
-
-    def set_callback(self, callback):
-        self.callback = callback
-        
-    def set_direction(self, direction):
-        self.direction = direction
-
-    def set_grace_period(self, grace_period):
-        self.grace_period = grace_period
-
-    def set_min_inter_alarm(self, min_inter_alarm):
-        self.min_inter_alarm = min_inter_alarm
-        
     def check_value(
         self,
         value,
-        threshold=None,
-        direction=None,
+        threshold_met,
         grace_period=None,
         min_inter_alarm=None,
     ):
-        if threshold is not None:
-            self.threshold = threshold
-
-        if direction is not None:
-            self.direction = direction
-
         if grace_period is not None:
             self.grace_period = grace_period
 
         if min_inter_alarm is not None:
             self.min_inter_alarm = min_inter_alarm
 
-        if self._threshold_met(value) is False:
+        if self.threshold_met(value) is False:
             self.initial_trigger_time = None
             return False
-        
+
         else:
             if self._check_grace_period() and self._check_min_inter_alarm():
                 self._trigger_alarm()
 
-    def _threshold_met(self, value):
-        if self.direction == "greater_than":
-            if value > self.threshold:
-                return True
-        elif self.direction == "less_than":
-            if value < self.threshold:
-                return True
-        return False
-
     def _check_grace_period(self):
-
         if self.initial_trigger_time is None:
             self.initial_trigger_time = time.time()
             return False
@@ -121,14 +82,13 @@ class Alarm:
         self.initial_trigger_time = None
 
 
-def check_alarm(
-    threshold=None,
+def create_alarm(
+    func,
+    threshold_met,
     callback=None,
-    direction=None,
     grace_period=None,
     min_inter_alarm=None,
 ):
-
     """A decorator to check an alarm against the return value of a function
     The function will fire the inputted callback if the alarm is triggered
     See below for an example of how to use this decorator
@@ -145,38 +105,20 @@ def check_alarm(
     -------
     """
 
-    def decorator(func):
-        ## Generate an id for the alarm instance
-        alarm_id = id(func)
+    alarm = Alarm(
+        threshold_met=threshold_met,
+        callback=callback,
+        grace_period=grace_period,
+        min_inter_alarm=min_inter_alarm,
+    )
 
-        @wraps(func)
-        async def wrapper(*args, **kwargs):
-            ## Get self from the first argument (assuming this decorates instance methods)
-            self = args[0]
-            
-            ## Instantiate the alarm if it doesn't exist
-            ## This allows multiple instances of the same function to have separate alarms
-            if not hasattr(self, "_alarm_instances"):
-                self._alarm_instances = {}
-            if alarm_id not in self._alarm_instances:
-                self._alarm_instances[alarm_id] = Alarm(
-                    threshold=threshold,
-                    callback=callback,
-                    direction=direction,
-                    grace_period=grace_period,
-                    min_inter_alarm=min_inter_alarm,
-                )
+    async def wrapper(*args, **kwargs):
+        result = await func(*args, **kwargs)
 
-            _alarm = self._alarm_instances[alarm_id]
+        alarm.check_value(result, threshold_met)
 
-            wrapper._alarm = _alarm
+        return result
 
-            result = await func(*args, **kwargs)
+    wrapper.alarm = alarm
 
-            _alarm.check_value(result)
-
-            return result
-
-        return wrapper
-
-    return decorator
+    return wrapper
