ab97020b443e5e7c5bf5d4d14c24d59d03a6ead7
mathsman5133 <mathsman5132@gmail.com>
2025-09-02 18:01:07 +1000

    bring loop definition out of run_app to store it between invocations

--- pydoover/cloud/processor/handler.py
+++ pydoover/cloud/processor/handler.py
@@ -6,19 +6,20 @@ from typing import Any
 from .application import Application
 
 
+# should probably fact check this but I don't think we can run asyncio.run in a lambda because it
+# recycles the environment...
+try:
+    loop = asyncio.get_running_loop()
+except RuntimeError:
+    loop = asyncio.new_event_loop()
+
+
 def run_app(
     app: Application, event: dict[str, Any], _context, setup_logging: bool = True
 ):
     if setup_logging:
         logging.basicConfig(level=logging.INFO)
         logging.getLogger().setLevel("INFO")
-
-    # should probably fact check this but I don't think we can run asyncio.run in a lambda because it
-    # recycles the environment...
-    try:
-        loop = asyncio.get_running_loop()
-    except RuntimeError:
-        loop = asyncio.new_event_loop()
 
     for record in event["Records"]:
         data = json.loads(record["Sns"]["Message"])
