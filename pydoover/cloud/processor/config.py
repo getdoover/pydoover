dbc64b1748341e4e94e3c05971b3964ac3006144
mathsman5133 <mathsman5132@gmail.com>
2025-10-23 13:28:30 +1100

    add config element to allow many subscription configs to be passed in

--- pydoover/cloud/processor/config.py
+++ pydoover/cloud/processor/config.py
@@ -1,12 +1,28 @@
-from ...config import String, Integer
+from ...config import String, Integer, Array
+
+
+class ManySubscriptionConfig(Array):
+    def __init__(
+        self,
+        display_name: str = "Subscription",
+        *,
+        description: str = "A list of channels to subscribe to.",
+        **kwargs,
+    ):
+        element = SubscriptionConfig()
+        element._name = "dv_proc_subscription"
+        super().__init__(
+            display_name, element=element, description=description, **kwargs
+        )
+        self._name = "dv_proc_subscriptions"
 
 
 class SubscriptionConfig(String):
     def __init__(
         self,
-        display_name: str = "Subscription",
+        display_name: str = "Channel Subscription",
         *,
-        description: str = "The name of the channel to subscribe to.",
+        description: str = "The name of the channel to subscribe to",
         **kwargs,
     ):
         super().__init__(display_name, description=description, **kwargs)
